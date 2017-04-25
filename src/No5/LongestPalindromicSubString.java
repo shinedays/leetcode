d6a310749c9d618cccd8ee6815437baaadc5d598
shinedays <jfwang1993@163.com>
2017-04-25 05:16:20 -0400

    update No5

--- src/No5/LongestPalindromicSubString.java
+++ src/No5/LongestPalindromicSubString.java
@@ -4,16 +4,19 @@ package No5;
  * Created by bupt on 4/25/17.
  */
 public class LongestPalindromicSubString {
-    public String longestPalindrome(String s){
+    public String longestPalindrome(String s) {
+
+        if (s.length() == 0)
+            return "";
         int length = s.length();
         boolean[][] dp = new boolean[length][length];
         int start = 0;
         int maxLength = 0;
 
-        for(int i = 0;i< length;i++){
+        for (int i = 0; i < length; i++) {
             dp[i][i] = true;
-            if(i < length -1 && s.charAt(i) == s.charAt(i+1)){
-                dp[i][i+1] = true;
+            if (i < length - 1 && s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
                 start = i;
                 maxLength = 2;
             }
@@ -21,13 +24,13 @@ public class LongestPalindromicSubString {
 
         boolean flag = true;
 
-        for(int strlen = 3;strlen <= length;strlen ++,flag = true){
-            for(int i = 0;i<= length -strlen;i++){
-                int j = i + strlen -1;
-                if(dp[i+1][j-1] == true && s.charAt(i) == s.charAt(j)){
+        for (int strlen = 3; strlen <= length; strlen++, flag = true) {
+            for (int i = 0; i <= length - strlen; i++) {
+                int j = i + strlen - 1;
+                if (dp[i + 1][j - 1] == true && s.charAt(i) == s.charAt(j)) {
                     dp[i][j] = true;
                     maxLength = strlen;
-                    if(flag == true){
+                    if (flag == true) {
                         start = i;
                         flag = false;
                     }
@@ -36,14 +39,14 @@ public class LongestPalindromicSubString {
             }
         }
 
-        if(maxLength > 0)
-            return s.substring(start,start + maxLength);
+        if (maxLength > 0)
+            return s.substring(start, start + maxLength);
 
-        return s.substring(0,1);
+        return s.substring(0, 1);
     }
 
     public static void main(String[] args) {
-        String s = "abba";
+        String s = "";
         System.out.println(new LongestPalindromicSubString().longestPalindrome(s));
     }
 }
