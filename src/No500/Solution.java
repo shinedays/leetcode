dd8aaa125438b5e7b4333ce27c00f823f166ff66
shinedays <jfwang1993@163.com>
2017-06-09 05:23:00 -0400

    No500

---
+++ src/No500/Solution.java
@@ -0,0 +1,77 @@
+package No500;
+
+import java.util.*;
+
+/**
+ * Created by bupt on 6/9/17.
+ */
+public class Solution {
+    public static String[] findWords(String[] words){
+        Set<Character> firstLine = new HashSet<Character>(){{
+            add('q');
+            add('w');
+            add('e');
+            add('r');
+            add('t');
+            add('y');
+            add('u');
+            add('i');
+            add('o');
+            add('p');
+        }};
+
+        Set<Character> secondLine = new HashSet<Character>(){{
+            add('a');
+            add('s');
+            add('d');
+            add('f');
+            add('g');
+            add('h');
+            add('j');
+            add('k');
+            add('l');
+        }};
+
+        Set<Character> thirdLine = new HashSet<Character>(){{
+            add('z');
+            add('x');
+            add('c');
+            add('v');
+            add('b');
+            add('n');
+            add('m');
+        }};
+
+        List<String> list = new LinkedList<>();
+
+        flag:for(String word:words){
+            String smallWord = word.toLowerCase();
+            char firstChar = smallWord.charAt(0);
+            Set set;
+            if(firstLine.contains(firstChar)){
+                set = firstLine;
+            } else if(secondLine.contains(firstChar))
+                set = secondLine;
+            else
+                set = thirdLine;
+
+            for(int i = 1;i < smallWord.length();i++){
+                char character = word.charAt(i);
+                if(!set.contains(character))
+                    continue flag;
+            }
+            list.add(word);
+        }
+
+
+        return list.toArray(new String[list.size()]);
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"Hello","Alaska","Dad","Peace"};
+        String[] res = findWords(input);
+        for(String s:res){
+            System.out.println(s);
+        }
+    }
+}
